4f9938ebc28b8236e598c50e4fd3d8c68fe75ee1
stevekutz <stkutz@gmail.com>
2020-07-21 21:28:06 -0500

    added 2 simple & 1 inclusion custom template tags

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,11 +1,25 @@
 from django import template
 from ..models import Post
 
+import datetime
+
 register = template.Library()
+
+format_string = "%b %x %X %Z %p"
 
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+@register.simple_tag(name = 'time')
+def show_current_time():
+     return datetime.datetime.now().strftime(format_string)
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
 
 
     
