7cab00817d1c7733cbc9e4c5d56722b5e9b9ce4f
stevekutz <stkutz@gmail.com>
2020-07-28 15:07:01 -0500

    added QuerySet examples to README

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Comment
 
+
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length = 25)
     email = forms.EmailField()
@@ -11,3 +12,5 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
+
+
